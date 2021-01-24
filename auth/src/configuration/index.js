28affa7aa28b402a8fc9af7a0357d7f855ba56df
Sergey <komarovs33@mail.ru>
2021-01-24 23:07:29 +0300

    add check routes and network

--- auth/src/configuration/index.js
+++ auth/src/configuration/index.js
@@ -1,3 +1,4 @@
 module.exports.port = process.env.PORT;
 module.exports.host = process.env.HOST;
 module.exports.db = process.env.MONGO_URL;
+module.exports.apiUrl = process.env.API_URL;
--- auth/src/index.js
+++ auth/src/index.js
@@ -1,12 +1,27 @@
 const express = require("express");
+const axios = require("axios");
 
-const { port, host, db } = require("./configuration");
+const { port, host, db, apiUrl } = require("./configuration");
 const { connectDb } = require("./helpers/db");
 
 const app = express();
 
 app.get("/test", (req, res) => {
   res.send("Our authentication server is working correctly");
+});
+
+app.get("/api/currentUser", (req, res) => {
+  res.json({ id: "1234", email: "foo@gmail.com" });
+});
+
+app.get("/testwithapidata", (req, res) => {
+  axios.get(apiUrl + "/testapidata").then((response) => {
+    res
+      .json({
+        testapidata: response.data.testapidata,
+      })
+      .catch((e) => console.log(e.toString()));
+  });
 });
 
 const startServer = () => {
